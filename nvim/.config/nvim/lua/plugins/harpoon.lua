479322f5acbe9a8be06443a65d1fca0c45d04868
Dylan Ullrich <dylan@eldyl.org>
2024-08-21 10:24:08 -0700

    Notes for future task

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -35,6 +35,30 @@ return {
       end)
     end
 
+    -- Allow telescope to view harpooned
+    -- local conf = require("telescope.config").values
+    -- local function toggle_telescope(harpoon_files)
+    --   local file_paths = {}
+    --   for _, item in ipairs(harpoon_files.items) do
+    --     table.insert(file_paths, item.value)
+    --   end
+    --
+    --   require("telescope.pickers")
+    --     .new({}, {
+    --       prompt_title = "Harpoon",
+    --       finder = require("telescope.finders").new_table({
+    --         results = file_paths,
+    --       }),
+    --       previewer = conf.file_previewer({}),
+    --       sorter = conf.generic_sorter({}),
+    --     })
+    --     :find()
+    -- end
+    --
+    -- vim.keymap.set("n", "<C-h>", function()
+    --   toggle_telescope(harpoon:list())
+    -- end, { desc = "Open harpoon window" })
+
     --https://github.com/ThePrimeagen/harpoon/issues/352#issuecomment-1893131934
     function Harpoon_files()
       local contents = {}
