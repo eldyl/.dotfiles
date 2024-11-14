ba6d53161f71fc6ab484d0bd1512bf2ce121ee7d
Dylan Ullrich <dylan@eldyl.org>
2024-11-14 10:15:47 -0800

    Fix: update config to work with new version of Guard

--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -4,16 +4,16 @@ return {
   dependencies = {
     "nvimdev/guard-collection",
   },
-  cmd = { "GuardFmt" },
+  cmd = { "Guard fmt" },
   keys = {
-    { "<leader>f", "<cmd>GuardFmt<cr>", desc = "Format Code" },
+    { "<leader>f", "<cmd>Guard fmt<cr>", desc = "Format Code" },
   },
   config = function()
     local ft = require("guard.filetype")
 
-    ft("typescript,javascript,typescriptreact,javascriptreact,vue,svelte,json,jsonc,yaml"):fmt(
-      "prettier"
-    )
+    ft(
+      "typescript,javascript,typescriptreact,javascriptreact,vue,svelte,json,jsonc,yaml"
+    ):fmt("prettier")
 
     ft("astro"):fmt({
       cmd = "pnpm",
@@ -33,14 +33,15 @@ return {
 
     ft("rust"):fmt("rustfmt")
 
-    ft("java"):fmt("google-java-format")
+    -- ft("java"):fmt("google-java-format")
 
-    -- Call setup() LAST!
-    require("guard").setup({
-      -- the only options for the setup function
+    vim.g.guard_config = {
+      -- format on write to buffer
       fmt_on_save = false,
-      -- Use lsp if no formatter was defined for this filetype
+      -- use lsp if no formatter was defined for this filetype
       lsp_as_default_formatter = false,
-    })
+      -- whether or not to save the buffer after formatting
+      save_on_fmt = true,
+    }
   end,
 }
