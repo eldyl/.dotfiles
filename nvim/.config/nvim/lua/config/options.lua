d41aac0e68d0768cc9491809b7a2c468ad13a4ef
Dylan Ullrich <dylan@eldyl.org>
2024-12-05 06:43:24 -0800

    Improved var name makes actions more clear

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,48 +1,48 @@
-local opt = vim.opt
+local set = vim.opt
 
 -- Map leader key
 vim.g.mapleader = " "
 
 -- File
-opt.swapfile = false
-opt.undofile = true
+set.swapfile = false
+set.undofile = true
 
 -- Clipboard
-opt.clipboard = "unnamedplus"
+set.clipboard = "unnamedplus"
 
 -- Visual
-opt.guicursor = ""
-opt.inccommand = "split"
-opt.signcolumn = "yes"
-opt.cursorline = true
+set.guicursor = ""
+set.inccommand = "split"
+set.signcolumn = "yes"
+set.cursorline = true
 
-opt.number = true
-opt.relativenumber = true
-opt.smartindent = true
-opt.wrap = false
-opt.showtabline = 2
-opt.splitbelow = true
-opt.splitright = true
-opt.updatetime = 250 -- faster updates
-opt.scrolloff = 10 -- show at least this many lines above or below
+set.number = true
+set.relativenumber = true
+set.smartindent = true
+set.wrap = false
+set.showtabline = 2
+set.splitbelow = true
+set.splitright = true
+set.updatetime = 250 -- faster updates
+set.scrolloff = 10 -- show at least this many lines above or below
 
 -- Tabs
-opt.autoindent = true
-opt.cindent = true
+set.autoindent = true
+set.cindent = true
 
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.softtabstop = 4
-opt.expandtab = true
+set.tabstop = 4
+set.shiftwidth = 4
+set.softtabstop = 4
+set.expandtab = true
 
 -- Search
-opt.ignorecase = true -- when searching
-opt.smartcase = true -- ... unless capital letter in query
+set.ignorecase = true -- when searching
+set.smartcase = true -- ... unless capital letter in query
 
 -- Hide .DS_Store files
-opt.wildignore:append(".DS_Store")
+set.wildignore:append(".DS_Store")
 
 -- words connected with '-' will count as a whole word
-opt.iskeyword:append("-")
+set.iskeyword:append("-")
 -- Change symbol for diffview
-opt.fillchars:append({ diff = "╱" })
+set.fillchars:append({ diff = "╱" })
