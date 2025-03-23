92f62d8a88f7f4c5511833e48cb1a6232e7d748a
Dylan Ullrich <dylan@eldyl.org>
2025-03-23 12:18:42 -0700

    Remove unused

--- nvim/.config/nvim/lua/config/autocommands.lua
+++ nvim/.config/nvim/lua/config/autocommands.lua
@@ -1,27 +1,27 @@
 -- Add column when in insert mode
-vim.api.nvim_create_autocmd("InsertEnter", {
-  desc = "Creates column at 80 char width when in insert mode",
-  group = vim.api.nvim_create_augroup("insert-mode-column", { clear = true }),
-  callback = function()
-    vim.schedule(function()
-      vim.opt.colorcolumn = "80"
-    end)
-  end,
-})
-
--- Remove column when leaving insert mode
-vim.api.nvim_create_autocmd("InsertLeave", {
-  desc = "Removes column at 80 char width when leaving insert mode",
-  group = vim.api.nvim_create_augroup(
-    "leave-insert-no-column",
-    { clear = true }
-  ),
-  callback = function()
-    vim.schedule(function()
-      vim.opt.colorcolumn = "0"
-    end)
-  end,
-})
+-- vim.api.nvim_create_autocmd("InsertEnter", {
+--   desc = "Creates column at 80 char width when in insert mode",
+--   group = vim.api.nvim_create_augroup("insert-mode-column", { clear = true }),
+--   callback = function()
+--     vim.schedule(function()
+--       vim.opt.colorcolumn = "80"
+--     end)
+--   end,
+-- })
+--
+-- -- Remove column when leaving insert mode
+-- vim.api.nvim_create_autocmd("InsertLeave", {
+--   desc = "Removes column at 80 char width when leaving insert mode",
+--   group = vim.api.nvim_create_augroup(
+--     "leave-insert-no-column",
+--     { clear = true }
+--   ),
+--   callback = function()
+--     vim.schedule(function()
+--       vim.opt.colorcolumn = "0"
+--     end)
+--   end,
+-- })
 
 -- Highlight when yanking
 vim.api.nvim_create_autocmd("TextYankPost", {
