21bf994362d5374eac0b3afce5b4dba005ddf0bd
Dylan Ullrich <dylan@eldyl.org>
2025-06-22 18:34:45 -0700

    simplified

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -209,7 +209,7 @@ return {
           -- "rust_analyzer",
           "emmet_language_server",
           "ts_ls",
-          "denols",
+          -- "denols",
           "astro",
           "svelte",
           "pyright",
@@ -218,141 +218,12 @@ return {
           "yamlls",
         },
 
+        automatic_enable = true,
         automatic_installation = false,
 
         handlers = {
           function(server_name)
             require("lspconfig")[server_name].setup({})
-          end,
-
-          -- rust_analyzer = function()
-          --   return true
-          -- end,
-          -- rust_analyzer = function()
-          --   require("lspconfig").rust_analyzer.setup({
-          --     -- Other Configs ...
-          --     settings = {
-          --       ["rust-analyzer"] = {
-          --         -- Other Settings ...
-          --         cargo = {
-          --           features = "all",
-          --         },
-          --         procMacro = {
-          --           ignored = {
-          --             leptos_macro = {
-          --               -- optional: --
-          --               -- "component",
-          --               "server",
-          --             },
-          --           },
-          --         },
-          --       },
-          --     },
-          --   })
-          -- end,
-
-          emmet_language_server = function()
-            require("lspconfig").emmet_language_server.setup({
-              filetypes = {
-                "css",
-                "eruby",
-                "html",
-                "javascript",
-                "javascriptreact",
-                "less",
-                "sass",
-                "scss",
-                "pug",
-                "typescriptreact",
-              },
-              -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
-              -- **Note:** only the options listed in the table are supported.
-              init_options = {
-                ---@type table<string, string>
-                includeLanguages = {},
-                --- @type string[]
-                excludeLanguages = {},
-                --- @type string[]
-                extensionsPath = {},
-                --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
-                preferences = {},
-                --- @type boolean Defaults to `true`
-                showAbbreviationSuggestions = true,
-                --- @type "always" | "never" Defaults to `"always"`
-                showExpandedAbbreviation = "always",
-                --- @type boolean Defaults to `false`
-                showSuggestionsAsSnippets = false,
-                --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
-                syntaxProfiles = {},
-                --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
-                variables = {},
-              },
-            })
-          end,
-
-          denols = function()
-            require("lspconfig").denols.setup({
-              on_attach = function(client)
-                if
-                  require("lspconfig").util.root_pattern(
-                    "deno.json",
-                    "import_map.json"
-                  )(vim.fn.getcwd())
-                then
-                  if client.name == "ts_ls" then
-                    client.stop()
-                    return
-                  end
-                end
-              end,
-              root_dir = require("lspconfig").util.root_pattern(
-                "deno.json",
-                "deno.jsonc"
-              ),
-            })
-          end,
-
-          -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/configure-volar-v2.md
-          ts_ls = function()
-            local vue_typescript_plugin = require("mason-registry")
-              .get_package("vue-language-server")
-              :get_install_path() .. "/node_modules/@vue/language-server" .. "/node_modules/@vue/typescript-plugin"
-
-            require("lspconfig").ts_ls.setup({
-              on_attach = function(client)
-                if
-                  require("lspconfig").util.root_pattern(
-                    "deno.json",
-                    "import_map.json"
-                  )(vim.fn.getcwd())
-                then
-                  if client.name == "ts_ls" then
-                    client.stop()
-                    return
-                  end
-                end
-              end,
-              root_dir = require("lspconfig").util.root_pattern("package.json"),
-              single_file_support = false,
-              init_options = {
-                plugins = {
-                  {
-                    name = "@vue/typescript-plugin",
-                    location = vue_typescript_plugin,
-                    languages = { "javascript", "typescript", "vue" },
-                  },
-                },
-              },
-              filetypes = {
-                "javascript",
-                "javascriptreact",
-                "javascript.jsx",
-                "typescript",
-                "typescriptreact",
-                "typescript.tsx",
-                "vue",
-              },
-            })
           end,
 
           lua_ls = function()
@@ -398,7 +269,7 @@ return {
           -- javascript = { "clsx%(([^)]+)%)" },
         },
       },
-    }, -- your configuration
+    },
   },
 
   {
