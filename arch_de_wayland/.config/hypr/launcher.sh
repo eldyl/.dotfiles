0e9f7fd37089f71c5f1d32dd8a66f76c5543a75f
Dylan Ullrich <dylan@eldyl.org>
2025-10-22 07:23:27 -0700

    allow assign client name if found doesen't match * Sometimes the client name doesn't match up with the info we need, so   this becomes a safeguard to easily maintain the functionality I want

--- arch_de_wayland/.config/hypr/launcher.sh
+++ arch_de_wayland/.config/hypr/launcher.sh
@@ -2,6 +2,13 @@
 
 workspace=$1
 app=$2
+client_name=$3
 
-hyprctl dispatch workspace $workspace
-hyprctl clients | grep -qi $app || hyprctl dispatch exec "[workspace $workspace] $app"
+hyprctl dispatch workspace "$workspace"
+clients=$(hyprctl clients)
+
+if [ -n "$client_name" ]; then
+    echo "$clients" | grep -qi "$app" || echo "$clients" | grep -qi "$client_name" || hyprctl dispatch exec "[workspace $workspace] $app"
+else
+    echo "$clients" | grep -qi "$app" || hyprctl dispatch exec "[workspace $workspace] $app"
+fi
